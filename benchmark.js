7b70337b35fd490ddf4e6837352531ca840b3bb7
ericdum <formateric@gmail.com>
2015-01-17 18:41:03 +0800

    benchmark

---
+++ benchmark.js
@@ -0,0 +1,26 @@
+var ab = require('ab');
+var db = require('./')({dir: '/tmp/smbh'});
+var expect = require('expect.js')
+
+var i = 0
+var fn = function (callback) {
+  db.increase('benchmark_increase', 1, function(){
+    if( i++ >= 5) {
+      i = 0
+      db.popAll(function(err, data){
+        expect(err).to.not.be.ok()
+        expect(data.benchmark_increase).above(4)
+        callback()
+      })
+    } else {
+      callback()
+    }
+  })
+};
+
+db.ready(function(){
+  ab.run(fn, {
+      concurrency: 5,
+      requests: 100000
+  });
+});
